12fc6c97801d601400f506443e6aa1cc1e873edb
nenadnesta <nenadnesta@gmail.com>
2020-08-04 16:18:33 +0200

    Moving Login Component into User Folder.

--- BookWorm/ClientApp/src/app/app.module.ts
+++ BookWorm/ClientApp/src/app/app.module.ts
@@ -18,7 +18,7 @@ import { OrderSuccessComponent } from './order-success/order-success.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './user/login/login.component';
 import { RegistrationComponent } from './user/registration/registration.component';
 
 @NgModule({
