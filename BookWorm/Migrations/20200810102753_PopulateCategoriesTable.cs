8d69471815f438bd8ce43964260bda8e960cb36e
nenadnesta <nenadnesta@gmail.com>
2020-08-10 12:44:05 +0200

    Seeding Categories Table.

---
+++ BookWorm/Migrations/20200810102753_PopulateCategoriesTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookWorm.Migrations
+{
+    public partial class PopulateCategoriesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categories (ValueName, DisplayName) VALUES ('romance', 'Best Romance Books')");
+            migrationBuilder.Sql("INSERT INTO Categories (ValueName, DisplayName) VALUES ('history', 'Best History Books')");
+            migrationBuilder.Sql("INSERT INTO Categories (ValueName, DisplayName) VALUES ('fantasy', 'Classic Fantasy Books')");
+            migrationBuilder.Sql("INSERT INTO Categories (ValueName, DisplayName) VALUES ('graphic', 'Best Graphic Novels')");
+            migrationBuilder.Sql("INSERT INTO Categories (ValueName, DisplayName) VALUES ('crime', 'Best Crime and Thriller Books')");
+            migrationBuilder.Sql("INSERT INTO Categories (ValueName, DisplayName) VALUES ('folktales', 'Best Fairytales nad Folktales')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
