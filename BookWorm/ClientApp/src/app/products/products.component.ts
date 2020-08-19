d125014ff32d46d4a3fc66280a081a74b615f4b7
nenadnesta <nenadnesta@gmail.com>
2020-08-19 19:51:22 +0200

    ProductsComponent - Multiple Async Operations.

--- BookWorm/ClientApp/src/app/products/products.component.ts
+++ BookWorm/ClientApp/src/app/products/products.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { CategoryService } from '../services/category.service';
 import { ActivatedRoute } from '@angular/router';
-import { Book } from '../models/Book';
+import { Book, Category } from '../models/Book';
+import { of, defer } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-products',
@@ -12,18 +14,92 @@ import { Book } from '../models/Book';
 export class ProductsComponent {
   products: Book[] = [];
   filteredProducts: Book[] = [];
-  categories;
+  categories: Category[] = [];
   category: string;
 
   constructor(
     private route: ActivatedRoute,
     private productService: ProductService,
     private categoryService: CategoryService) {
+
+    // this.productService.getAll()
+    //   .subscribe(
+    //     (products: any) => {
+    //       this.products = products;
+    //       this.categoryService.getAll()
+    //         .subscribe(categories => this.categories = categories);
+    //       this.route.queryParamMap
+    //         .subscribe(params => {
+    //           this.category = params.get('category');
+
+    //           if (this.category) {
+    //             this.productService.getProductsByCategory(this.category)
+    //               .subscribe(fProducts => this.filteredProducts = fProducts);
+    //           } else {
+    //             this.filteredProducts = this.products;
+    //           }
+    //         },
+    //           err => {
+    //             console.log(err);
+    //           }
+    //         );
+    //     });
+
+
+
+    // this.productService.getAll()
+    //   .pipe(
+    //     switchMap(products => {
+    //       this.products = products;
+    //       return this.categoryService.getAll()
+    //         .pipe(
+    //           map(categories => this.categories = categories)
+    //         );
+    //     }),
+    //     switchMap(() => this.route.queryParamMap
+    //       .pipe(
+    //         switchMap(params => {
+    //           this.category = params.get('category');
+    //           return defer(() =>
+    //             (Boolean(this.category) ?
+    //               this.productService.getProductsByCategory(this.category) :
+    //               of(this.products)
+    //             )
+    //           );
+    //         })
+    //       )
+    //     )
+    //   )
+    //   .subscribe((filteredProducts) => {
+    //     this.filteredProducts = filteredProducts;
+    //   },
+    //     err => {
+    //       console.log(err);
+    //     }
+    //   );
+
+
+
     this.productService.getAll()
-      .subscribe(
-        (products: any) => {
+      .pipe(
+        switchMap(products => {
           this.products = products;
-        },
+          return this.route.queryParamMap
+            .pipe(
+              switchMap(params => {
+                this.category = params.get('category');
+                return defer(() =>
+                (Boolean(this.category) ?
+                  this.productService.getProductsByCategory(this.category) :
+                  of(this.products)
+                ));
+              })
+            );
+        })
+      )
+      .subscribe((filteredProducts) => {
+        this.filteredProducts = filteredProducts;
+      },
         err => {
           console.log(err);
         }
@@ -31,17 +107,6 @@ export class ProductsComponent {
 
     this.categoryService.getAll()
       .subscribe(categories => this.categories = categories);
-
-    // here we cannot use snapshot
-    this.route.queryParamMap.subscribe(params => {
-      this.category = params.get('category');
-
-      if (this.category) {
-        this.productService.getProductsByCategory(this.category).subscribe(products => this.filteredProducts = products);
-      } else {
-        this.filteredProducts = this.products;
-      }
-    });
 
   }
 
