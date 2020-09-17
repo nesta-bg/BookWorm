f486cf47ca6daaae0f4014be088a8b0c69d6f676
nenadnesta <nenadnesta@gmail.com>
2020-09-17 19:34:02 +0200

    ShoppingCartComponent - Get Total Price.

--- BookWorm/ClientApp/src/app/models/ShoppingCart.ts
+++ BookWorm/ClientApp/src/app/models/ShoppingCart.ts
@@ -14,17 +14,17 @@ export class ShoppingCart {
 
   }
 
-  get totalItemsCount() {
-    let count = 0;
-    for (let id in this.shoppingCartItems)
-      count += this.shoppingCartItems[id].quantity;
-    return count;
-  }
-
   get totalPrice() {
     let sum = 0;
     for (let id in this.shoppingCartItems)
       sum += this.shoppingCartItems[id].totalPrice;
     return sum;
+  }
+
+  get totalItemsCount() {
+    let count = 0;
+    for (let id in this.shoppingCartItems)
+      count += this.shoppingCartItems[id].quantity;
+    return count;
   }
 }
