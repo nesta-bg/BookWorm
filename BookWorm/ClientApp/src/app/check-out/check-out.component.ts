0d0a8d912dc7cee6fc63795e80db133795df6e38
nenadnesta <nenadnesta@gmail.com>
2020-10-08 19:49:22 +0200

    CheckOutComponent - Building a Shipping Form.

--- BookWorm/ClientApp/src/app/check-out/check-out.component.ts
+++ BookWorm/ClientApp/src/app/check-out/check-out.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-check-out',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./check-out.component.css']
 })
 export class CheckOutComponent implements OnInit {
+  shippingForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.shippingForm = this.fb.group({
+      name: ['', Validators.required],
+      address1: ['', Validators.required],
+      address2: ['', Validators.required],
+      city: ['', Validators.required]
+    });
+  }
+
+  get name() {
+    return this.shippingForm.get('name');
+  }
+  get address1() {
+    return this.shippingForm.get('address1');
+  }
+  get address2() {
+    return this.shippingForm.get('address2');
+  }
+  get city() {
+    return this.shippingForm.get('city');
+  }
+
+  placeOrder() {
+    console.log(this.shippingForm.value);
   }
 
 }
